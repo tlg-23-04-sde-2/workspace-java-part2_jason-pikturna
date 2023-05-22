99cfe08b628efc6fc42db7b99f48fc7694466733
Jason Pikturna <Jason.Pikturna@tlgcohort.com>
2023-05-22 07:25:38 -0700

    Scratch;  revisiting sorting.

--- Scratch/src/com/crisp/RadishSortTest.java
+++ Scratch/src/com/crisp/RadishSortTest.java
@@ -1,6 +1,7 @@
 package com.crisp;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 class RadishSortTest {
@@ -19,6 +20,26 @@ class RadishSortTest {
         radishes.sort(new RadishColorComparator());
         System.out.println("Color");
         dump(radishes);
+        System.out.println();
+
+        System.out.println("Sort by tailLength, via anonymous Comparator");
+        radishes.sort(new Comparator<Radish>() {
+
+            @Override
+            public int compare(Radish r1, Radish r2) {
+                return Double.compare(r1.getTailLength(), r2.getTailLength());
+            }
+        });
+        dump(radishes);
+        System.out.println();
+
+        System.out.println("Sort by leaves, via anonymous Comparator");
+        radishes.sort(new Comparator<Radish>() {
+            @Override
+            public int compare(Radish r1, Radish r2) {
+                return Integer.compare(r1.getLeaves(), r2.getLeaves());    // switch r1,r2 to get DESCENDING ORDER
+            }
+        });
     }
 
     private static void dump(List<Radish> radishList) {
